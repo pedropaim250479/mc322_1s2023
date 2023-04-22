082c317d9e1cba3b2df0b49ca306f49300c37518
pedropaim250479 <127906831+pedropaim250479@users.noreply.github.com>
2023-04-22 15:55:14 -0300

    Add files via upload

---
+++ Lab 02/bin/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int numero = 10;
+        int sum = numero+1;
+        System.out.println("Hello, World!");
+        System.out.println(sum);
+    }
+}
